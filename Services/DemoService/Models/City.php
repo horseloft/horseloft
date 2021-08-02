54d353f0de3ad90a52fab67ab8b112cba48a304d
YHC <yanhaochuang@qq.com>
2021-08-02 18:37:52 +0800

    新增调试SQLserver类

---
+++ Services/DemoService/Models/City.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Date: 2021/8/2 17:21
+ * User: YHC
+ * Desc:
+ */
+
+namespace Services\DemoService\Models;
+
+use Library\Pool\Table;
+
+class City extends Table
+{
+    public static $table = 'city';
+
+    public static $connection = 'main';
+}
