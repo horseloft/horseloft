f4cf68439973d8aebd4653bdbf3b505dbcb5e522
YHC <yanhaochuang@qq.com>
2021-10-18 17:12:33 +0800

    修改ini的读取

--- Core/core.php
+++ Core/core.php
@@ -1,15 +1,4 @@
 <?php
-/*
- * --------------------------------------------------------------------------
- * 【读取env文件并定义当前运行环境】
- * --------------------------------------------------------------------------
- *
- */
-if (!is_file(dirname(__DIR__) . '/env.php')) {
-    die('env文件不存在');
-}
-
-require_once dirname(__DIR__) . '/env.php';
 
 $loader = require_once dirname(__DIR__) . '/vendor/autoload.php';
 $loader->addPsr4('Application' . '\\', dirname(__DIR__));
@@ -20,4 +9,4 @@ $loader->addPsr4('Application' . '\\', dirname(__DIR__));
  * --------------------------------------------------------------------------
  *
  */
-$_HORSELOFT_CORE_CONTAINER_ = new \Horseloft\Core\Drawer\Horseloft();
+$_HORSELOFT_CORE_CONTAINER_ = new \Horseloft\Core\Drawer\Building();
--- env.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-const HORSELOFT = 'dev';
-
-const HTTP_HOST = '0.0.0.0';
